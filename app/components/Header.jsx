97b9fd557fe3bdb79b946f0f764fd4ed6e86224c
zohaib-babar <zohaibbabar3388@gmail.com>
2025-11-26 19:34:09 +0500

    add text

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -2,10 +2,10 @@ export default function Header() {
   return (
     <div>
       <div className="justify-center items-center text-center">
-        <h1 className="text-3xl text-gray-800 font-bold mt-10">Muhammad Zohaib Babar</h1>
+        <h1 className="text-3xl text-gray-800 font-bold mt-10">𝕄𝕦𝕙𝕒𝕞𝕞𝕒𝕕 𝕫𝕠𝕙𝕒𝕚𝕓 𝔹𝕒𝕓𝕒𝕣</h1>
         <img className="text-gray-600 h-[200px] w-[200px] rounded-4xl mt-5 text-center ml-[330px]" src="https://media-mct1-1.cdn.whatsapp.net/v/t61.24694-24/589052601_848405210887212_8150349523155917912_n.jpg?ccb=11-4&oh=01_Q5Aa3AHUj7rmO6OFFsJG0z-MwtQlSrCOiTiVpCssNbR3z0g_6Q&oe=692FE331&_nc_sid=5e03e0&_nc_cat=108" alt="zohaib pic" />
       </div>
-      <p className="text-gray-600 text-center mt-5 text-2xl">Frontened Developer</p>
+      <p className="text-black text-center mt-5 text-3xl">𝔽𝕣𝕠𝕟𝕥𝕖𝕟𝕕 𝔻𝕖𝕧𝕖𝕝𝕠𝕡𝕖𝕣</p>
     </div>
   );
 }
