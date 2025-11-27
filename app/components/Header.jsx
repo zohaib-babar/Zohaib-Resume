8e58b74291fce028d1ebd6199983f1be2227d70e
zohaib-babar <zohaibbabar3388@gmail.com>
2025-11-27 18:11:38 +0500

    set image

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -3,7 +3,7 @@ export default function Header() {
     <div>
       <div className="justify-center items-center text-center">
         <h1 className="text-3xl text-gray-800 font-bold mt-10">𝕸𝖚𝖍𝖆𝖒𝖒𝖆𝖉 𝖅𝖔𝖍𝖆𝖎𝖇 𝕭𝖆𝖇𝖆𝖗</h1>
-        <img className="text-gray-600 h-[200px] w-[200px] rounded-full mt-5 text-center ml-[330px]" src="https://media-mct1-1.cdn.whatsapp.net/v/t61.24694-24/589052601_848405210887212_8150349523155917912_n.jpg?ccb=11-4&oh=01_Q5Aa3AHUj7rmO6OFFsJG0z-MwtQlSrCOiTiVpCssNbR3z0g_6Q&oe=692FE331&_nc_sid=5e03e0&_nc_cat=108" alt="zohaib pic" />
+        <img className="text-gray-600 h-[200px] w-[200px] rounded-full mt-5 block mx-auto" src="https://media-mct1-1.cdn.whatsapp.net/v/t61.24694-24/589052601_848405210887212_8150349523155917912_n.jpg?ccb=11-4&oh=01_Q5Aa3AHUj7rmO6OFFsJG0z-MwtQlSrCOiTiVpCssNbR3z0g_6Q&oe=692FE331&_nc_sid=5e03e0&_nc_cat=108" alt="zohaib pic" />
       </div>
       <p className="text-black text-center mt-5 text-3xl">𝕱𝖗𝖔𝖓𝖙𝖊𝖓𝖉 𝕯𝖊𝖛𝖊𝖑𝖔𝖕𝖊𝖗</p>
     </div>
