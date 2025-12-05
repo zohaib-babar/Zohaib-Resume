de5e54dc956a24e8e6b47505bf69b0c2daddf629
zohaib-babar <zohaibbabar3388@gmail.com>
2025-12-05 21:28:23 +0500

    Add image

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -3,7 +3,7 @@ export default function Header() {
     <div>
       <div className="justify-center items-center text-center">
         <h1 className="text-3xl text-white font-bold p-5">𝕸𝖚𝖍𝖆𝖒𝖒𝖆𝖉 𝖅𝖔𝖍𝖆𝖎𝖇 𝕭𝖆𝖇𝖆𝖗</h1>
-        <img className="h-[200px] w-[200px] rounded-full border-amber-100 border-5  block mx-auto" src="https://media-mct1-1.cdn.whatsapp.net/v/t61.24694-24/589052601_848405210887212_8150349523155917912_n.jpg?ccb=11-4&oh=01_Q5Aa3AHUj7rmO6OFFsJG0z-MwtQlSrCOiTiVpCssNbR3z0g_6Q&oe=692FE331&_nc_sid=5e03e0&_nc_cat=108" alt="zohaib pic" />
+        <img className="h-[200px] w-[200px] rounded-full border-amber-100 border-5  block mx-auto" src="https://media-mct1-1.cdn.whatsapp.net/v/t61.24694-24/589052601_848405210887212_8150349523155917912_n.jpg?ccb=11-4&oh=01_Q5Aa3AHJHJf4Gy3ICfoH1llUla5uEv8FW2A89YjBCYU3947mUA&oe=694025B1&_nc_sid=5e03e0&_nc_cat=108" alt="zohaib pic" />
       </div>
       <p className="text-blue-600 text-center mt-5 text-3xl">𝕱𝖗𝖔𝖓𝖙𝖊𝖓𝖉 𝕯𝖊𝖛𝖊𝖑𝖔𝖕𝖊𝖗</p>
     </div>
